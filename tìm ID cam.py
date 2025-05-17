76d1bad9be2964fa2787d0aae05adf2f6c8a9fc4
Đinh Viết Phúc <159451818+vodanh404@users.noreply.github.com>
2025-05-17 20:31:01 +0700

    Update tìm ID cam.py
    Signed-off-by: Đinh Viết Phúc <159451818+vodanh404@users.noreply.github.com>

--- tìm ID cam.py
+++ tìm ID cam.py
@@ -1,15 +1,19 @@
 import cv2
 
 def tim_id_webcam():
-    """Tìm kiếm và in ra các ID webcam có thể."""
+    """Tìm kiếm và in ra các ID webcam có thể. Báo nếu không tìm thấy."""
     index = 0
+    found_camera = False
     while True:
         cap = cv2.VideoCapture(index)
         if not cap.read()[0]:
             cap.release()
+            if index == 0 and not found_camera:
+                print("Không tìm thấy webcam nào.")
             break
         else:
             print(f"Tìm thấy webcam tại ID: {index}")
+            found_camera = True
             cap.release()
         index += 1
 
